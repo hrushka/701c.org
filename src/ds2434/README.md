e64202c8afe95e64e91ea2e04a950d2f2b7aceab
matt <matt@hru.sh>
2022-10-18 18:12:36 -0700

    Initial commit of the DS2434 clone for the 701C/CS

---
+++ src/ds2434/README.md
@@ -0,0 +1,60 @@
+# 🦋 701c.org / Battery / DS2434 Clone  
+
+Find the full rebuild and walkthrough at [https://701c.org/projects/battery](https://701c.org/projects/battery).
+
+> **Note**: If you'd rather purchase a pre-built version of the board with the IC already programmed, you can do so from the [701c.org Shop](https://701c.org/shop).
+
+#
+
+## Background
+
+The following is a clone of an obsolete 1-wire IC (Dallas DS2434) that any ThinkPad 701 battery needs in order to be recognized by the system and allowed to charge.
+
+There is a lot of non-standard behavior that make this chip unique and why purchasing a DS2434 off eBay or Aliexpress won't get you very far.
+
+It has been tested using an Arduino Uno developent board and can be compiled and run on an **ATtiny85** MCU. It uses the internal temperature sensing and recieves +3.3V power from the laptop so this is the only chip needed, making PCB design super straight forward.
+
+This code requires a modified version of the awesome OneWireHub library by [Ingmar Inʒo Splitt (@orgua)](https://github.com/orgua) that you can find here: 
+
+[https://github.com/hrushka/OneWireHub](https://github.com/hrushka/OneWireHub)
+
+#
+
+## Building the clone
+
+1. Download [the code](https://github.com/hrushka/701c.org/tree/main/src/ds2434/ds2434.ino) and open it in the Arduino IDE.
+   
+2. Download the modified version of the [OneWireHub](https://github.com/hrushka/OneWireHub) as a Zip file.
+   
+3. In your Arduino IDE, go into *Sketch → Include Library →  Add .ZIP Library...* and select the downloaded zip file.
+   
+4. Open up your preferences and under "*Additional Boards Manager URLs*" add: https://raw.githubusercontent.com/damellis/attiny/ide-1.6.x-boards-manager/package_damellis_attiny_index.json
+
+5. Close Preferences and under the *Tools* menu select the following:
+   1. **Board**: ATtiny 25/45/85
+   2. **Processor**: ATtiny85
+   3. **Clock**: Internal 16mhz
+   4. **Port**: [*your programmer*]
+
+6. In order to configure the ATtiny and set the clock to 16mhz, select "*Burn bootloader*".
+
+7. Once this is complete, you should be able to upload the code directly to the ATtiny without any additional setup.
+
+> **Note**: The ATtiny version of the code measures the temperature by checking the voltage resistance and uses a baseline. Depending on your chip/package selection you may want to calibrate the values in the **chipTemp()** function. Instructions on how to do this can be found here: http://www.atmel.com/Images/doc8108.pdf
+
+#
+
+## The PCB
+
+
+If you'd like to print your own PCB, you can [download the latest Gerber files here](https://github.com/hrushka/701c.org/tree/main/assets/battery/gerber). There is also a version that will work with the original DS2434 IC in case you happen to have one you've pulled form an original battery.
+
+The PCB uses the **ATTINY85-20SF** surface mount package
+
+#
+
+## Things to know...
+
+1. Any new battery/cells need to be calibrated. Before the ThinkPad will show an accurate charge level or function properly, it will need to go through a full charge/discharge cycle. Charge the battery fully until the green light turns off on its own, then unplug and let the battery fully discharge. Repeat the charge cycle and you should be good to go. You can press **FN+F2** at any time to see the current charge and estimated time left.
+
+2. The battery has been tested with **Ni-Cad** and **Ni-MH** type batteries. I do not know how the chip or laptop will behave if you use another chemistry like **Lithium-ion**. All of the reference chips I used were pulled from original **Ni-Cad** batteries.
---
+++ src/ds2434/ds2434.ino
@@ -0,0 +1,186 @@
+/*
+*   This code emulates a DS2434 with Battery Management, Temperature & EEPROM
+*   It was designed specifically for use with an IBM ThinkPad 701c Battery (or rebuild)
+*
+*   Tested with
+*    - 1995 IBM ThinkPad 701CS as Master, Arduino Uno and attiny85@16MHz as Slave
+*
+*   Author: hrushka (701c.org)
+*   Required: https://github.com/hrushka/OneWireHub - a modified version of the original
+*   
+*   Special Thanks: Ingmar Inʒo Splitt (@orgua) for updates to the OneWireHub framework
+*   and initial code for the DS2434.
+*/
+
+#include "OneWireHub.h" // https://github.com/hrushka/OneWireHub
+#include "DS2434.h"
+
+#if defined(ARDUINO_AVR_UNO)
+constexpr uint8_t pin_onewire   { 7 }; // for attiny
+#else
+constexpr uint8_t pin_onewire   { 2 }; // for Arduino Uno
+#endif
+
+constexpr uint8_t tempCheckInterval = 5000;
+unsigned long     lastTempCheck     = 0; 
+unsigned long     time_now          = 0;
+uint8_t           lastTemp          = 20;
+
+auto hub = OneWireHub(pin_onewire);
+auto ds2434 = DS2434(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00); // Serial & family not used for DS2434
+
+/*
+ * We need to initialize the memory for the read of the chip and
+ * set up for internal temp sensing.
+ */
+void setup()
+{
+    // Setup OneWire
+    hub.attach(ds2434);
+
+    // Check to see if we should restore memory from EEPROM.
+    if(!ds2434.checkRestoreMemory()){
+
+        // No memory was found in the EEPROM, load defaults
+        constexpr uint8_t mem1[24] = {0x14, 0x10, 0x90, 0xd0, 0x03, 0x32, 0x4b, 0x3c,
+                                  0xff, 0x04, 0x64, 0x04, 0x9e, 0x9a, 0x3a, 0xf0,
+                                  0x00, 0x00, 0x04, 0xee, 0x63, 0xB8, 0x3E, 0x63 };
+        ds2434.writeMemory(reinterpret_cast<const uint8_t *>(mem1),sizeof(mem1),0x00);
+
+        constexpr  uint8_t mem2[8] = {0x33, 0x2e, 0x33, 0x2e, 0x9e, 0x10, 0x3f, 0x50 };
+        ds2434.writeMemory(reinterpret_cast<const uint8_t *>(mem2),sizeof(mem2),0x20);
+
+        ds2434.setID(0xCABDu);
+        ds2434.setBatteryCounter(0x0101u);
+
+        // Tell the system to save the defaults to EEPROM
+        ds2434.persistAllMemory();
+    }
+
+    ds2434.unlockNV1();
+
+    // Setup the Analog to Digital Converter for internal temp sensing
+    ADCSRA &= ~(_BV(ADATE) |_BV(ADIE)); // Clear auto trigger and interrupt enable
+    ADCSRA |= _BV(ADEN);                // Enable AD and start conversion
+    ADMUX = 0xF | _BV( REFS1 );         // ADC4 (Temp Sensor) and Ref voltage = 1.1V;
+    delay(100);                         // Settling time min 1 ms, take 100 ms
+    getADC();
+}
+
+void loop()
+{
+    // following function must be called periodically
+    hub.poll();
+
+    if (ds2434.getTemperatureRequest()){
+
+      time_now = millis();
+
+      /*
+       * Only check for a new temperature every so often to mimize blocking.
+       * All other times, report back the last temp recorded.
+       */
+      if(time_now >= (lastTempCheck + tempCheckInterval)){
+          uint8_t temp = getInternalTemp();
+          ds2434.setTemperature(temp);
+          lastTempCheck = time_now;
+          lastTemp = temp;
+      } else {
+          ds2434.setTemperature(lastTemp);
+      }
+    }
+
+
+    hub.poll();
+
+    // Check to see if we need to persist the memory
+    ds2434.checkPersistMemory(hub);
+}
+
+/* 
+ * getInternalTemp() Uses the internal temperature sensor of the attiny85
+ * Tested with ~3.3V VCC on the ThinkPad 701C laptop
+ * Adapted from: https://21stdigitalhome.blogspot.com/2014/10/trinket-attiny85-internal-temperature.html
+*/
+int8_t getInternalTemp(){
+
+  // Arduino doesn't have the same ability. Return back a static temp.
+  #if defined(ARDUINO_AVR_UNO)
+  return 20;
+  #else
+
+  int i;
+  int t_celsius; 
+  uint8_t vccIndex;
+  float rawTemp, rawVcc;
+  
+  // Measure temperature
+  ADCSRA |= _BV(ADEN);           // Enable AD and start conversion
+  ADMUX = 0xF | _BV( REFS1 );    // ADC4 (Temp Sensor) and Ref voltage = 1.1V;
+  
+  // Pause for settling time, but keep polling to pick up reset/presence polls
+  delayWhilePolling(100);
+
+  rawTemp = (float)getADC();     // use next sample as initial average
+  for (int i=2; i<2000; i++) {   // calculate running average for 2000 measurements
+    rawTemp += ((float)getADC() - rawTemp) / float(i); 
+  }  
+  ADCSRA &= ~(_BV(ADEN));        // disable ADC  
+
+  // Measure chip voltage (Vcc)
+  ADCSRA |= _BV(ADEN);           // Enable ADC
+  ADMUX  = 0x0c | _BV(REFS2);    // Use Vcc as voltage reference,
+                                 //    bandgap reference as ADC input
+
+  // Pause for settling time, but keep polling to pick up reset/presence polls
+  delayWhilePolling(100);
+                                
+  rawVcc = (float)getADC();      // use next sample as initial average
+  for (int i=2; i<2000; i++) {   // calculate running average for 2000 measurements
+    rawVcc += ((float)getADC() - rawVcc) / float(i);
+  }
+  ADCSRA &= ~(_BV(ADEN));        // disable ADC
+  
+  rawVcc = 1024 * 1.1f / rawVcc;
+  //index 0..13 for vcc 1.7 ... 3.0
+  vccIndex = min(max(17,(uint8_t)(rawVcc * 10)),30) - 17;   
+
+  /*
+   * Temperature compensation using the chip voltage 
+   * The 701c measures 3.22 V going to the DS2434 
+   */
+  t_celsius = (int)(chipTemp(rawTemp) + (float)vccIndex / 13); 
+
+  return t_celsius;
+  #endif
+}
+
+/*
+ * Delay the current procedure while still polling so that no requests/resets/presence 
+ * are dropped in the process.
+ */
+ void delayWhilePolling(uint8_t delay){
+    time_now = millis(); // deliberate reuse of the global variable
+    while(millis() < time_now + delay){
+      hub.poll();
+    }
+ }
+
+/* 
+ * Calibration of the temperature sensor has to be changed for your own ATtiny85
+ * per tech note: http://www.atmel.com/Images/doc8108.pdf
+*/
+float chipTemp(float raw) {
+  const float chipTempOffset = 282.9;           // Your value here, it may vary 
+  const float chipTempCoeff  = 1.075;           // Your value here, it may vary
+  return((raw - chipTempOffset) / chipTempCoeff);
+}
+
+/*
+ * Common code for both sources of an ADC conversion
+ */
+int getADC() {
+  ADCSRA  |=_BV(ADSC);            // Start conversion
+  while((ADCSRA & _BV(ADSC)));    // Wait until conversion is finished
+  return ADC;
+}
