7f537a76f40b3ffb660e6be107dbe4d017b37f99
matt <matt@hru.sh>
2022-10-26 19:33:46 -0700

    Fixed pin assignment

--- src/ds2434/ds2434.ino
+++ src/ds2434/ds2434.ino
@@ -19,9 +19,9 @@
 #include "DS2434.h"
 
 #if defined(ARDUINO_AVR_UNO)
-constexpr uint8_t pin_onewire   { 2 }; // for Arduino Uno
+constexpr uint8_t pin_onewire   { 7 }; // for Arduino Uno
 #else
-constexpr uint8_t pin_onewire   { 7 }; // for attiny
+constexpr uint8_t pin_onewire   { 2 }; // for attiny
 #endif
 
 constexpr uint8_t tempCheckInterval = 5000;
